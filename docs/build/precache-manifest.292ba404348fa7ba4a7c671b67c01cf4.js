5ac7d2a655c01cb7d835d4288e42e67696e88a84
mkhs <mkh_sharif@aol.com>
2019-02-25 12:45:05 -0500

    add build folder to docs

---
+++ docs/build/precache-manifest.292ba404348fa7ba4a7c671b67c01cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "200ccdbc96b176fcaade",
+    "url": "/static/css/main.5b0e575a.chunk.css"
+  },
+  {
+    "revision": "200ccdbc96b176fcaade",
+    "url": "/static/js/main.200ccdbc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ce3051e049f3fdade3f",
+    "url": "/static/js/2.5ce3051e.chunk.js"
+  },
+  {
+    "revision": "c00e4b4b7b587d0f3103a0960abae148",
+    "url": "/index.html"
+  }
+];
